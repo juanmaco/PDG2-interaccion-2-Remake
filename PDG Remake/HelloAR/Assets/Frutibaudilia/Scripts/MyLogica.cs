da45b0fc9a9f405d7c5489d51836bc380cd6032f
Juan Manuel Córdoba <juanmaco-97@hotmail.com>
2019-01-03 16:47:21 -0500

    Remake de la interaccion
    funciones basicas del juego con escenario armado

---
+++ PDG Remake/HelloAR/Assets/Frutibaudilia/Scripts/MyLogica.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sample { 
+public class MyLogica : MonoBehaviour {
+        public OnClick[] myButtons;
+        public List<int> colorList;
+
+        public float showTime = 0.5f;
+        public float pauseTime =0.2f;
+
+        bool maquina = false;
+        public bool player = false;
+        bool gameO = false;
+
+        private int myRandom;
+        public int level = 1;
+        private int playerLevel;
+        
+
+	// Use this for initialization
+	void Start () {
+            playerLevel = 0;
+            maquina = true;
+            for(int i=0; i<myButtons.Length; i++)
+            {
+                myButtons[i].onClick += ButtonClicked;
+                myButtons[i].myNumber = i;
+            }
+	}
+        void ButtonClicked(int _number)
+        {
+            if (player == true)
+            {
+                if (_number == colorList[playerLevel])
+                {
+                    playerLevel += 1;
+                }
+                if(playerLevel == level)
+                {
+                    level += 1;
+                    playerLevel = 0;
+                    maquina = true;
+                }
+            }
+        }
+	
+	// Update is called once per frame
+	void Update () {
+            if (maquina)
+            {
+                player = false;
+                maquina = false;
+                StartCoroutine(Robot());
+            }
+            if(player == false && gameO == false)
+            {
+               //vacio legal 
+            }
+            if(player== true)
+            {
+                //vacio legal2
+            }
+	}
+        IEnumerator Robot()
+        {
+            for(int i =0; i<level; i++)
+            {
+                if( colorList.Count < level)
+                {
+                    myRandom = Random.Range(0, myButtons.Length);
+                    colorList.Add(myRandom);
+                }
+                yield return new WaitForSeconds(0.3f);
+                myButtons[colorList[i]].ClickedColor();
+                myButtons[colorList[i]].mySound.Play();
+                yield return new WaitForSeconds(showTime);
+                myButtons[colorList[i]].UnclickedColor();
+                yield return new WaitForSeconds(pauseTime);
+            }
+            player = true;
+        }
+}
+}
---
+++ PDG Remake/HelloAR/Assets/Frutibaudilia/Scripts/OnClick.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sample { 
+public class OnClick : MonoBehaviour {
+    public Material lMaterial;
+    public Material nMaterial;
+    private Renderer mReneder;
+    private Vector3 myTp;
+    public int myNumber = 99;
+    public delegate void ClickEv(int number);
+    public MyLogica gameLog;
+    public AudioSource mySound;
+    public SampleImageTargetBehaviour myTarget;
+        bool seeActivo;
+
+        public event ClickEv onClick;
+      
+        // Use this for initialization
+        void Start () {
+            mReneder = GetComponent<Renderer>();
+            mReneder.enabled=true;
+            myTp = transform.position;
+            seeActivo = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+        public void OnMouseDown()
+        {
+           if(gameLog.player == true)
+            {
+                ClickedColor();
+                transform.position = new Vector3(myTp.x, myTp.y, myTp.z);
+                onClick.Invoke(myNumber);
+                StartCoroutine(Sound());
+
+            }
+        }
+        public void OnMouseUp()
+        {
+            UnclickedColor();
+            transform.position = new Vector3(myTp.x, myTp.y, myTp.z);
+        }
+        public void ClickedColor()
+        {
+            mReneder.sharedMaterial = lMaterial;
+            transform.position = new Vector3(myTp.x, myTp.y , myTp.z);
+        }
+        public void UnclickedColor()
+        {
+            mReneder.sharedMaterial = nMaterial;
+            transform.position = new Vector3(myTp.x, myTp.y, myTp.z);
+        }
+        IEnumerator Sound()
+        {
+            mySound.Play();
+            yield return new WaitForSeconds(.5f);
+        }
+    }
+}
